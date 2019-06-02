9f504719d9e67e882aeb6e069a0e31564a97b3b3
William Peterson <william.o.peterson@gmail.com>
2019-06-01 17:54:57 -0700

    Update name and deck
    Changed the styling of the name and deck
    
    Began working on animation

--- public/javascripts/gbls.js
+++ public/javascripts/gbls.js
@@ -1,12 +1,34 @@
 async function setScreen(){
     var response = await fetch('/screen');
     var data = await response.json();
+    var nameElement = document.getElementById("name");
     
     console.log(data);
     console.log(data.Image);
     console.log(data.Deck);
     document.getElementById("main").style.backgroundImage = "url('" + data.Image + "')";
-    document.getElementById("name").innerHTML = data.Name + "&nbsp;";
+    // await animateOutLeft(nameElement);
+
+    // var pos = 0;
+    // var endWidth = -nameElement.offsetWidth;
+    // var id = await setInterval(frame, 5);
+    // var isAnimating = true;
+    // function frame(){
+    //     if(pos == endWidth){
+    //         clearInterval(id);
+    //         isAnimating = false;
+    //     }
+    //     else{
+    //         pos--;
+    //         nameElement.style.left = pos + "px";
+    //     }
+    // }
+
+    // while(isAnimating){
+    //     //Don't do anything while the animation is going.
+    // }
+
+    nameElement.innerHTML = data.Name + "&nbsp; <img src='../images/favicon.ico'/>";
     document.getElementById("deck").innerHTML = data.Deck;
 }
 
@@ -19,4 +41,21 @@ async function nextScreen(){
     await setScreen();
 
     return setTimeout(nextScreen, 10000);
+}
+
+async function animateOutLeft(element){
+    var width = element.offsetWidth;
+    console.log(width);
+
+    var id = await setInterval(await frame(element, 0, -width, id), 5);
+}
+
+async function frame(element, currentPosition, stopPosition, id){
+    if(currentPosition == stopPosition){
+        clearInterval(id);
+    }
+    else{
+        currentPosition--;
+        element.style.left = currentPosition + "px";
+    }
 }

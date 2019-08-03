8ec9a050c44f446f2643807c4ff7b3a18d3f1731
William Peterson <william.o.peterson@gmail.com>
2019-08-03 14:33:18 -0700

    Change in getting load screens
    Instead of randomly choosing a category then randomly choosing an item from there, get the total number of possible results, generate a random number based on that, then figure out which category that belongs to and get the appropriate result.
    
    While getting the total number of results - which can take a while currently  - we'll still get screens the old way so we can display them.  Once the categories are filled out we'll have fewer calls to GiantBomb and should weight what's being displayed more to categories that have more items, hopefully reducing the repetition in the smaller categories.

--- routes/index.js
+++ routes/index.js
@@ -1,37 +1,167 @@
 var express = require('express');
 var router = express.Router();
 var path = require('path');
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  // res.render('index', { title: 'Express' });
-  res.sendFile('gbls.html', {root: path.join(__dirname, '../public')});
-});
-
-router.get('/screen', async function(req, res, next){
-  var screen = await getLoadingScreen();
-  res.send(screen);
-})
+const fetch = require('node-fetch');
+var _loadingScreenHelper;
 
 module.exports = router;
 
 
-class Category{
-  constructor(singular, plural){
-      this.Singular = singular;
-      this.Plural = plural;
+class Category {
+  constructor(singular, plural) {
+    this.Singular = singular;
+    this.Plural = plural;
+    this.Size = 0;
   }
 }
 
-class LoadingScreen{
-  constructor(name, image, deck){
+class LoadingScreen {
+  constructor(name, image, deck) {
     this.Name = name;
     this.Image = image;
     this.Deck = deck;
   }
 }
 
-const fetch = require('node-fetch');
+class LoadingScreenHelper{
+  constructor(categories = [], totalResults = 0){
+    this.Categories = categories;
+    this.TotalResults = totalResults;
+    this.AreCategoriesInitialized = false;
+  }
+
+  async initCategories() {
+
+    for(var i = 0; i < this.Categories.length; i++){
+  
+      var category = this.Categories[i];
+      var response = await fetch("https://www.giantbomb.com/api/" + category.Plural + "/?api_key=121aa0789ad0e36deb7c4ff3feeeb2f46f9ac6b3&format=json");
+      
+      if(response.ok){
+        var data = await response.json();
+        category.Size = parseInt(data.number_of_total_results, 10);
+        this.TotalResults += category.Size;
+      }
+      else{
+        i--;
+      }      
+    };
+
+    this.AreCategoriesInitialized = true;
+  }
+
+  async getLoadingScreen(){
+
+    var catNumber;
+    var category;
+    var response;
+    var result;
+
+    if(this.AreCategoriesInitialized){
+      catNumber = Math.floor(Math.random() * this.TotalResults) + 1;
+
+      var position = 0;
+
+      for(var i = 0; i < this.Categories.length; i++){
+
+        category = this.Categories[i];
+
+        if(catNumber <= position + category.Size){
+
+          var itemNumber;
+          var response;
+
+          do{
+            itemNumber = catNumber - position;
+            response = await fetch("https://www.giantbomb.com/api/" + category.Plural + "/?api_key=121aa0789ad0e36deb7c4ff3feeeb2f46f9ac6b3&format=json&limit=1&offset=" + itemNumber);  
+          }
+          while(!response.ok)
+          break;
+        }
+        else{
+          position += category.Size;
+        }        
+      }
+
+      var data = await response.json();
+
+      result = data.results[0];
+
+    }
+    else{
+      do {
+        catNumber = Math.floor(Math.random() * this.Categories.length);
+        category = this.Categories[catNumber];
+        response = await fetch("https://www.giantbomb.com/api/" + category.Plural + "/?api_key=121aa0789ad0e36deb7c4ff3feeeb2f46f9ac6b3&format=json"); //limit 1 works with every category except shows, using it with shows leave total number of results at 1
+      }
+      while (!response.ok);
+    
+      var data = await response.json();
+    
+      console.log("Results: " + data.number_of_total_results);
+    
+      var itemNumber;
+      var response2;
+    
+      do {
+        itemNumber = Math.floor(Math.random() * (data.number_of_total_results));
+        response2 = await fetch("https://www.giantbomb.com/api/" + category.Plural + "/?api_key=121aa0789ad0e36deb7c4ff3feeeb2f46f9ac6b3&format=json&limit=1&offset=" + itemNumber);
+      }
+      while (!response2.ok);
+    
+      var data2 = await response2.json();
+      console.log(data2.results)
+      result = data2.results[0];
+    }
+  
+  
+    var imageUrl = "https://www.giantbomb.com/api/image/scale_large/3026329-gb_default-16_9.png";
+    var name = result.name;
+    var deck = result.deck;
+  
+    if (name == null || name == undefined || name == "") {
+      name = result.title;
+    }
+  
+    if (deck == null || deck == undefined || deck == "") {
+      deck = result.name;
+    }
+  
+    if (result.image != null) {
+      imageUrl = result.image.super_url;
+    }
+  
+    console.log("Category: " + category.Singular);
+  
+    if (!["video", "video_show", "video_category"].includes(category.Singular)) {
+  
+      var imageResponse = await fetch("https://www.giantbomb.com/api/images/" + result.guid + "/?api_key=121aa0789ad0e36deb7c4ff3feeeb2f46f9ac6b3&format=json");
+      var imageData;
+  
+      if (imageResponse.ok) {
+        imageData = await imageResponse.json();
+  
+        console.log("image results: " + imageData.results.length);
+  
+        if (imageData.results.length > 0) {
+          var imageNumber = Math.floor(Math.random() * (imageData.results.length));
+  
+          console.log(imageData.results);
+          console.log(imageData.results[imageNumber].super_url);
+  
+          imageUrl = imageData.results[imageNumber].super_url;
+        }
+  
+      }
+    }
+  
+    var screen = new LoadingScreen(name, imageUrl, deck);
+  
+    return screen;
+  }
+}
+
+
 
 const categories = [
   new Category("accessory", "accessories"),
@@ -43,7 +173,7 @@ const categories = [
   new Category("genre", "genres"),
   new Category("location", "locations"),
   new Category("object", "objects"),
-  new Category("person", "people"),
+  // new Category("person", "people"), //This category is pretty empty in terms of images and decks
   new Category("platform", "platforms"),
   new Category("theme", "themes"),
   new Category("video", "videos"),
@@ -51,83 +181,23 @@ const categories = [
   new Category("video_show", "video_shows")
 ]
 
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  // res.render('index', { title: 'Express' });
+  _loadingScreenHelper = new LoadingScreenHelper(categories);
+  _loadingScreenHelper.initCategories();
+  res.sendFile('gbls.html', { root: path.join(__dirname, '../public') });
+});
+
+router.get('/screen', async function (req, res, next) {
+  var screen = await getLoadingScreen();
+  res.send(screen);
+})
+
+
 const getLoadingScreen = async () => {
 
-  var catNumber;
-  var category;
-  var response;
+  return await _loadingScreenHelper.getLoadingScreen();
 
-  do{
-    catNumber = Math.floor(Math.random() * categories.length);
-    category = categories[catNumber];  
-    response = await fetch("https://www.giantbomb.com/api/" + category.Plural + "/?api_key=121aa0789ad0e36deb7c4ff3feeeb2f46f9ac6b3&format=json"); //limit 1 works with every category except shows, using it with shows leave total number of results at 1
-  }
-  while (!response.ok);
-  
-  var data = await response.json();
-
-  console.log("Results: " + data.number_of_total_results);
-  
-  var itemNumber;
-  var response2; 
-  
-  do{
-    itemNumber = Math.floor(Math.random() * (data.number_of_total_results));
-    response2 = await fetch("https://www.giantbomb.com/api/" + category.Plural + "/?api_key=121aa0789ad0e36deb7c4ff3feeeb2f46f9ac6b3&format=json&limit=1&offset=" + itemNumber);
-  }
-  while(!response2.ok);
-  
-  var data2 = await response2.json();
-  console.log(data2.results)
-
-  var imageUrl = "https://www.giantbomb.com/api/image/scale_large/3026329-gb_default-16_9.png";
-  var name = data2.results[0].name;
-  var deck = data2.results[0].deck;
-
-  if(name == null || name == undefined || name == "")
-  {
-    name = data2.results[0].title;
-  }
-
-  if(deck == null || deck == undefined || deck == "")
-  {
-    deck = data2.results[0].name;
-  }
-
-  if(data2.results[0].image != null)
-  {
-    imageUrl = data2.results[0].image.super_url;
-  }
-
-  console.log("Category: " + category.Singular);
-
-  if(!["video", "video_show", "video_category"].includes(category.Singular))
-  {
-
-    var imageResponse = await fetch("https://www.giantbomb.com/api/images/" + data2.results[0].guid + "/?api_key=121aa0789ad0e36deb7c4ff3feeeb2f46f9ac6b3&format=json");
-    var imageData;
-
-    if(imageResponse.ok){
-      imageData = await imageResponse.json();
-  
-      console.log("image results: " + imageData.results.length);
-    
-      if(imageData.results.length > 0)
-      {
-        var imageNumber = Math.floor(Math.random() * (imageData.results.length));
-  
-        console.log(imageData.results);
-        console.log(imageData.results[imageNumber].super_url);
-      
-        imageUrl = imageData.results[imageNumber].super_url;
-      }
-  
-    }
-  }
-
-  var screen = new LoadingScreen(name, imageUrl, deck);
-
- 
-
-  return screen;
 }
